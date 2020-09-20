01665147f317157302a4a0dbd8ae5114c8158764
Jakub Konečný <jakub.konecny2@centrum.cz>
2020-09-20 15:31:16 +0200

    updated dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 require __DIR__ . "/../vendor/autoload.php";
 require_once(__DIR__ . "/../src/functions.php");
 
-Testbench\Bootstrap::setup(__DIR__ . '/_temp', function (\Nette\Configurator $configurator) {
+Testbench\Bootstrap::setup(__DIR__ . '/_temp', function (\Nette\Configurator $configurator): void {
   $configurator->addParameters(["appDir" => __DIR__,]);
   $configurator->addConfig(__DIR__ . "/tests.neon");
 });
