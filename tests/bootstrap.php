5b1d5c3d01adae2976beec415a96aac20dce744d
Jakub Konečný <jakub.konecny2@centrum.cz>
2024-08-30 21:53:31 +0200

    do not use deprecated stuff

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ require __DIR__ . "/../vendor/autoload.php";
 require_once(__DIR__ . "/../src/functions.php");
 
 Testbench\Bootstrap::setup(__DIR__ . '/_temp', function (\Nette\Configurator $configurator): void {
-  $configurator->addParameters(["appDir" => __DIR__,]);
+  $configurator->addStaticParameters(["appDir" => __DIR__,]);
   $configurator->addConfig(__DIR__ . "/tests.neon");
 });
 ?>
